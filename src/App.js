706295f2fd2a5d6da44855d74d635e1914515837
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-26 22:22:15 -0800

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserForm from './Components.js/UserForm';
+
+function App() {
+  return (
+    <div className="App">
+     <UserForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components.js/UserForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+
+export class UserForm extends Component {
+   state = {
+      step: 1,
+      firtsName: '',
+      lastName: '',
+      email: '',
+      occupation: '',
+      bio: '',
+      city: ''
+   }
+
+   //Proceed to next Step
+
+   nextStep = () => {
+      const {step} = this.state;
+      this.setState({
+         step: step + 1
+      });
+   }
+
+   //Go to pervious Step
+
+   previousStep = () => {
+      const {step} = this.state;
+      this.setState({
+         step: step - 1
+      });
+   }
+
+   //Handle fields change
+
+   handleChange = input => e => {
+      this.setState({[input]: e.target.value});
+   }
+
+
+   render() {
+      const { step } = this.state;
+      const { firstName, lastName, email, occupation,bio,
+      city } = this.state;
+      const values = { firstName, lastName, email, occupation, bio,
+      city }
+
+      switch(step) {
+         case 1:
+            return (
+               <FormUserDetails
+               nextStep={this.nextStep}
+               handleChange={this.handleChange}
+               values={values}/>
+            )
+
+         
+      }
+   }
+}
+
+export default UserForm
